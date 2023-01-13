1b0478b6e9a9dc89bc7a5059caa88b970b94edc6
cmSmith <cmsmith.ej@gmail.com>
2023-01-13 13:20:43 +0000

    Added comments to code for clarity

--- messageGenerator.js
+++ messageGenerator.js
@@ -1,13 +1,16 @@
+// Three arrays containing messages for combination later. 
 const firstMsg = ["Hey Buddy!" , "Hey there friend... ", "Alright mate?! ", "Top of the mornin' to ya. "];
 const secondMsg = ["How are you doing today? ", "What's crackin?? ", "What's loosey goosey? ", "U good? "];
 const thirdMsg = ["I'm Chris, nice to meet you", "I'm Dave, king of the hill!", "I'm Mike, the KING", "I'm Rich... shut up"];
 
+// function to take an array in and return a random index content. 
 const randMsg = arr => { 
     const randomIndex = Math.floor(Math.random() * arr.length);
     const item = arr[randomIndex];
     return item;
 };
 
+// display message to return to the user (combining all three random message selections). 
 const displayMessage = (`Your randomly generated message: 
                         ${randMsg(firstMsg)} ${randMsg(secondMsg)} ${randMsg(thirdMsg)}`);
 
